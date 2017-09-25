798e7e6201fde4af594a4552feaf0516b77b3e90
David Akers <david@quintify.com>
2017-09-25 15:59:19 -0400

    Updated HealthDRS to be able to spend Health Cash

--- test/HealthDRS-Token.js
+++ test/HealthDRS-Token.js
@@ -12,8 +12,8 @@ import isAddress from './helpers/isAddress'
 contract('HealthDRS :: Token', function(accounts) {
 
   beforeEach(async function() {
-    var startDateTime = new Date('2017-08-25T20:23:01.804Z').getTime()
-    var endDateTime = new Date('2017-10-25T20:23:01.804Z').getTime()    
+    var startDateTime = new Date('2017-12-25').getTime()/1000
+    var endDateTime = new Date('2018-1-25').getTime()/1000  
     this.token = await HealthCash.new(100, startDateTime, endDateTime, accounts[0])
     const healthDRSAddress = await this.token.healthDRS()
     this.drs = HealthDRS.at(healthDRSAddress)     
@@ -26,14 +26,14 @@ contract('HealthDRS :: Token', function(accounts) {
   })  
 
   it('should return the correct allowance after authorizing', async function() {
-    await this.token.authorizeHealthDRS(100);
+    await this.token.authorizeHealthDRS(100)
     const allowance = await this.drs.authorizedToSpend()
     allowance.should.be.bignumber.equal(100)  
   }) 
 
   it('should return the correct total after registering a gatekeeper', async function() {
-    await this.token.authorizeHealthDRS(1);
-    let key = await this.drs.registerGatekeeper('url-here');
+    await this.token.authorizeHealthDRS(1)
+    let key = await this.drs.registerGatekeeper('url-here')
 
     let balance = await this.token.balanceOf(accounts[0])
     balance.should.be.bignumber.equal(99,'Should have spent 1 to register a gatekeeper.')
