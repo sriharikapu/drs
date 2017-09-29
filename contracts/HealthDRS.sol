1941a5c3b71cf465f3a49d25e729614b9f253f10
David Akers <david@quintify.com>
2017-09-29 16:53:06 -0400

    added DRS datastructs, key creation, HealthDRS tests

--- contracts/HealthDRS.sol
+++ contracts/HealthDRS.sol
@@ -5,24 +5,203 @@ import './BurnableToken.sol';
 
 contract HealthDRS is Ownable {
 
+    /**
+    * Health Decentralized Record Service (DRS)
+    * This contract enables registration and permissioning
+    * of gatekeeper services using keys that can be managed,
+    * shared, or sold using Health Cash (HLTH). 
+    */
+
     BurnableToken public token;
     uint public registrationPrice = 1; 
+    uint16 public version = 1;     
+    address public latestContract = address(this);         
 
+    //Gatekeeper service only have a url for now
+    struct Gatekeeper {
+        string url;         
+    }
+
+    //Ring establishes relationships between keys
+    struct Ring {
+        uint8 distance;
+        Gatekeeper gatekeeper;
+        bytes32[] keys;
+    }
+    Ring[] rings;    
 
     struct Key {
-        bytes32 key;         
+        bytes32 id;         
         address owner;        
-        address authorizedToPurchase;
-        uint price;
-        //--
-        Ring ring; 
+        uint primary; 
+        uint secondary;        
     }
 
-    struct Ring {
-        bytes url;        
+    mapping(bytes32 => Key) keys;
+    mapping(address => bytes32[]) accountKeys;     
 
+    /* 
+        Create Keys
+            - Create a root key by registering a gatekeeper url
+            - Create a peer key 
+            - Create a child key
+            - Create a shared access key
+    */
+    event NewKey(bytes32 indexed _key);
+
+    //Create a root key by registering a gatekeeper url    
+    function registerGatekeeper(string _url) 
+        isAuthorizedToSpend(registrationPrice)    
+        returns (bytes32 id)
+    {
+       //HLTH spent to register are burned
+       token.transferFrom(msg.sender, address(this), registrationPrice);
+       token.burn(registrationPrice); 
+
+       //create a unique id 
+       id = keccak256(_url,now,msg.sender);
+
+       //make sure to not recreate existing keys
+       require(keys[id].primary == 0);
+
+       keys[id].id = id;       
+       keys[id].owner = msg.sender;       
+
+       //primary ring
+       Ring memory primary;
+       primary.distance = 0;
+       primary.gatekeeper.url = _url;
+       keys[id].primary = rings.push(primary) - 1;
+       rings[keys[id].primary].keys.push(id);
+
+       //secondary ring
+       Ring memory secondary;
+       secondary.distance = 1;
+       keys[id].secondary = rings.push(secondary) - 1;
+       rings[keys[id].secondary].keys.push(id);
+
+       accountKeys[msg.sender].push(id);
+       
+       NewKey(id);
     }
-    bytes32[] public keyList;
+
+    //Create a peer key from a key you own
+    function createPeerKey(bytes32 _id) returns (bytes32 id) {
+
+       require(msg.sender == keys[_id].owner);
+
+       //create a unique id 
+       id = keccak256(_id,now,msg.sender);
+
+       //make sure to not recreate existing keys
+       require(keys[id].primary == 0);
+
+       keys[id].id = id;       
+       keys[id].owner = msg.sender; 
+
+       //primary is the same
+       keys[id].primary = keys[_id].primary;
+       rings[keys[id].primary].keys.push(id);
+
+       //new secondary ring
+       Ring memory secondary;
+       secondary.distance = rings[keys[id].primary].distance + 1;
+       keys[id].secondary = rings.push(secondary) - 1;
+       rings[keys[id].secondary].keys.push(id);
+       accountKeys[msg.sender].push(id);
+
+       NewKey(id);
+    }
+
+    //Create a child key from a key you own 
+    function createChildKey(bytes32 _id) returns (bytes32 id) {
+
+       require(msg.sender == keys[_id].owner);
+
+       //create a unique id 
+       id = keccak256(_id,now,msg.sender);
+
+       //make sure to not recreate existing keys
+       require(keys[id].primary == 0);
+
+       keys[id].id = id;       
+       keys[id].owner = msg.sender; 
+
+
+       //primary is parent's secondary
+       keys[id].primary = keys[_id].secondary;
+       rings[keys[id].primary].keys.push(id);
+
+       //secondary ring
+       Ring memory secondary;
+       secondary.distance = rings[keys[id].primary].distance + 1;
+       keys[id].secondary = rings.push(secondary) - 1;
+       rings[keys[id].secondary].keys.push(id);
+
+       accountKeys[msg.sender].push(id);
+       NewKey(id);
+    }
+
+    // Create a clone key to be able to share access for a key you own
+    function createCloneKey(bytes32 _id) returns (bytes32 id) {
+
+       require(msg.sender == keys[_id].owner);
+
+       //create a unique id 
+       id = keccak256(_id,now,msg.sender);
+
+       //make sure to not recreate existing keys
+       require(keys[id].primary == 0);
+
+       keys[id].id = id;       
+       keys[id].owner = msg.sender; 
+
+       //primary is the same
+       keys[id].primary = keys[_id].primary;
+       rings[keys[id].primary].keys.push(id);   
+
+       //secondary is the same
+       keys[id].secondary = keys[_id].secondary;
+       rings[keys[id].secondary].keys.push(id);
+
+       accountKeys[msg.sender].push(id);
+
+       NewKey(id);
+    }
+
+    /** 
+    * Sell Keys
+    */
+
+    //Offers contain a key id, an address, and a purchase price (HLTH)
+    //They can only be created by a key owner
+    //To sell you create an offer, and the buyer has to purchase based on the offer 
+    //We need a way to retrieve offers made to an address
+    //We need to update the owners and the accountKeys mapping
+
+ 
+    /**
+    * Trade Keys
+    */
+
+    // Trade offer list will be a mapping of a keys you want
+    // with a give you are willing to trade 
+    // Where only the owner can add trade offers 
+    // When a trade offer is made - we'll look to see if 
+    // a matching offer exists and make the trade if so. 
+    // This needs to update accountKeys
+
+
+    /**
+    * Manage Keys
+    */
+    
+    //need to be able to remove keys
+    //first only allow parent keys to remove keys 
+    //peer keys can not remove each other
+    //if a key is removed we have to delete it from the accountsKey 
+    //mapping, the key mapping, and from any related rings
+
 
     /*
     * HLTH tokens can not be spent using HealthDRS 
@@ -38,25 +217,42 @@ contract HealthDRS is Ownable {
         return token.allowance(msg.sender, address(this)); 
     }
 
-    //Register a Gatekeeper by its URL
-    //Using Control Key 
-    //create an Access Key
-    //Set Price, Address of Valid Purchaser
-    //Purchaser 
 
-    function registerGatekeeper(bytes url) 
-    isAuthorizedToSpend(registrationPrice)
-    returns (bytes32) 
-    {
-       bytes32 key = keccak256(url);
 
-       //HLTH spent to register are burned
-       token.transferFrom(msg.sender, address(this), registrationPrice);
-       token.burn(registrationPrice); 
-
-       return key;
+    /**
+    * 
+    *  Utility functions for interacting with DRS
+    * 
+    */
+    function getKeyOwner(bytes32 id) constant returns (address) {
+        return keys[id].owner;
     }
-    
+
+    //Using any key retreive the gatekeeper's url
+    function getURL(bytes32 id)
+    constant returns (string)
+    {
+        if (rings[keys[id].primary].distance == 0) {
+            return rings[keys[id].primary].gatekeeper.url;
+        } else {
+            bytes32 keyId;
+            for (uint i = 0; i < rings[keys[id].primary].keys.length; i++) {
+                keyId = rings[keys[id].primary].keys[i];
+                if (rings[keys[keyId].primary].distance < rings[keys[id].primary].distance) {
+                    return getURL(keyId);
+                }
+            }
+        }
+    }
+
+    //Update a gatekeeper url using key
+    function updateGatekeeperUrl(bytes32 id, string url) {
+       require(msg.sender == keys[id].owner); //owner only
+       require(rings[keys[id].primary].distance == 0); //root key only
+       rings[keys[id].primary].gatekeeper.url = url;
+    }
+
+
     /**
     * [Admin Only] 
     * Allow admin access to tokens transfered to this 
---
+++ test/HealthDRS.js
@@ -0,0 +1,125 @@
+
+const BigNumber = web3.BigNumber
+const should = require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should()
+
+var HealthCashMock = artifacts.require('./helpers/HealthCashMock.sol');
+var HealthDRS = artifacts.require("./HealthDRS.sol")
+import isAddress from './helpers/isAddress'
+
+contract('HealthDRS', function(accounts) {
+
+  beforeEach(async function() {
+    this.token = await HealthCashMock.new()
+    this.drs = await HealthDRS.new()
+    await this.drs.setHealthCashToken(this.token.address)
+    await this.token.approve(this.drs.address, 1)    
+    this.url = 'https://blogs.scientificamerican.com/observations/consciousness-goes-deeper-than-you-think/'    
+  })
+  
+  it('should be able to create a gatekeeper', async function() {
+    let tx = await this.drs.registerGatekeeper('string')
+    let key = tx.logs[0].args._key
+    let owner = await this.drs.getKeyOwner(key)
+    owner.should.equal(accounts[0]);    
+  })
+ 
+  it('should return the correct URL when passed a root key', async function() {
+    let tx = await this.drs.registerGatekeeper(this.url)
+    let key = tx.logs[0].args._key
+    let url = await this.drs.getURL(key) 
+    url.should.equal(this.url)
+  })
+
+
+  it('should return the correct URL, when url is very long', async function() {
+    let longurl = 'http://www.longurlmaker.com/go?id=6bSitelutionsf4URL.co.ukLiteURLr1Shorl0ShortURLfar%2Boff5rk10rangylengthy2URL.co.uk50aFhURL0uShredURL1URLHawkh816Is.gd1f8drawn%2Bout98Sitelutions011far%2Breachingremote0enduringoutstretchedfcontinued0olengthened193URLviSmallr2faraway0NanoRef6807qarexpandedtuTraceURL1sRedirx150lengthy6URLremoteaFhURL0ag301URLgreatfCanURL0TinyURLdrawn%2Bout0outstretched7lengthenedTinyURLspun%2Bout0clnk.in25b5ovextensiveIs.gdyjremoteeenlarged16Minilien0Doiop22stringy0elongate0farawaytr0glengthenedx00CanURLlongish12NotLong317XiluganglingdX.se821i1remotee830wNanoRefMyURL09llastingFwdURL35URl.ieShoterLinkXil07expanded91TinyLink5far%2Breaching0ShortURLdenlarged90000fMetamark6farawayzzdistanttalln0elengthy1lasting07qelongatedcbhfarawaye6NotLonggreat9URL.co.uk174Shrtndlengthened119k31f43FwdURLlastings6d0SmallrsxURLCutter111SnipURLrangyspun%2BoutShortenURL2loftydrawn%2Boutq97l7lengthened6c9EasyURL0WapURLenduringoutstretchedWapURL805b5jfc2stretchrunning00ganglingFhURLMooURL11enduring8NutshellURL09a8tallMyURLtURl.ieSHurl11G8L9RubyURLURL.co.uklingeringspread%2Bouto40olofty1ShortenURLspun%2Bout2acNanoRefSimURL1cYATUCG8Lfar%2BreachingrrangyWapURL518URLvi0a1elongatestringyhighgNe1tall0011CanURLe115expanded8MyURL1021YATUCNanoRef07e1w1alengthy00YepIta0h0s081fSimURL6aTinyLink0Ne123301URL011running0c020111wfarawayNe1spun%2BoutEzURL4i5outstretchedTinyLink0MooURLseYATUC091sURLPie2e1enlargedb011pexpandedgreathighMinilien4rangy0ftowering111e83yrunninglofty417e4057cNutshellURL07URl.ie9m607lasting40remote176jy745URl.ielengthy81remote8spun%2Bout0prolonged30x88longishcvSimURL02elongate527d52m47dbdvfgnStartURL1pNutshellURLo5111gangling0316drawn%2BoutRedirx3147d81farawaydrawn%2Boutd1pdRedirxdistantURLHawkf61119102516stringy1enlarged00B65lengthyg0lofty53019URLcutMinilien4Smallrn761zTraceURL7j10distantIs.gddistant7l2running101796141zhighbwTightURL1105ShortenURLrangygfar%2Boff11great1Smallrstringy1G8Llengthy0061411c1longish0URLHawk01MooURL8remote000e13jspun%2Boutbsustained0p1extensivel11TraceURLlanky19stretching05m66x810005URLHawk63continuedc20ShortenURLrangymShortURL0u1112Redirxenduring9loftylen'
+    let tx = await this.drs.registerGatekeeper(longurl)
+    let key = tx.logs[0].args._key
+    let url = await this.drs.getURL(key) 
+    url.should.equal(longurl)
+  })
+
+
+  it('should be able to create a child key', async function() {
+    let tx1 = await this.drs.registerGatekeeper(this.url)
+    let key1 = tx1.logs[0].args._key
+    let tx2 = await this.drs.createChildKey(key1) 
+    tx2.logs[0].event.should.equal('NewKey')
+    let key2 = tx2.logs[0].args._key    
+    
+    let owner = await this.drs.getKeyOwner(key2)
+    owner.should.equal(accounts[0]);    
+  })
+
+
+  it('should return the correct URL when passed a child key', async function() {
+    let tx1 = await this.drs.registerGatekeeper(this.url)
+    let key1 = tx1.logs[0].args._key
+    let tx2 = await this.drs.createChildKey(key1) 
+    let key2 = tx2.logs[0].args._key  
+
+    let url = await this.drs.getURL(key2) 
+    url.should.equal(this.url)    
+  })
+
+
+  it('should be able to create a peer key', async function() {
+    let tx1 = await this.drs.registerGatekeeper(this.url)
+    let key1 = tx1.logs[0].args._key
+    let tx2 = await this.drs.createPeerKey(key1) 
+    tx2.logs[0].event.should.equal('NewKey')
+    let key2 = tx2.logs[0].args._key    
+    
+    let owner = await this.drs.getKeyOwner(key2)
+    owner.should.equal(accounts[0]);    
+  })
+
+
+  it('should return the correct URL when passed a peer key', async function() {
+    let tx1 = await this.drs.registerGatekeeper(this.url)
+    let key1 = tx1.logs[0].args._key
+    let tx2 = await this.drs.createPeerKey(key1) 
+    let key2 = tx2.logs[0].args._key  
+
+    let url = await this.drs.getURL(key2) 
+    url.should.equal(this.url)    
+  })
+
+
+  it('should update gatekeeper url when using a root key', async function() {
+    let tx = await this.drs.registerGatekeeper(this.url)
+    let key = tx.logs[0].args._key
+    
+    let tx2 = await this.drs.createChildKey(key) 
+    let key2 = tx2.logs[0].args._key  
+
+    await this.drs.updateGatekeeperUrl(key,'changedUrl')    
+
+    let url = await this.drs.getURL(key2) 
+    url.should.equal('changedUrl')    
+  })
+
+ 
+  it('should be able to clone a key you own', async function() {
+    let tx1 = await this.drs.registerGatekeeper(this.url)
+    let key1 = tx1.logs[0].args._key
+    
+    let tx2 = await this.drs.createChildKey(key1) 
+    let key2 = tx2.logs[0].args._key  
+    
+    let tx3 = await this.drs.createCloneKey(key1)
+    let key3 = tx3.logs[0].args._key
+
+    //changing the url with the cloned key should 
+    //effect all the child keys of the original keys
+    await this.drs.updateGatekeeperUrl(key3,'changedUrl')
+        
+    let url = await this.drs.getURL(key2) 
+    url.should.equal('changedUrl')        
+
+  })
+
+})
